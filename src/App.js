23e3bce6aeb4a6bece9ee2be856e36cebbdda0ca
szigetvarikati <szigetvarikati@gmail.com>
2024-04-02 17:05:35 +0200

    use SearchBar and PhotoTable components in App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import PhotoTable from './components/PhotoTable';
+import SearchBar from './components/SearchBar';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          <p>NAVA - Házi feladat</p>
+        </div>
       </header>
+      <main>
+        <SearchBar />
+        <PhotoTable />
+      </main>
+      <footer className="App-footer">
+        <p>Készítette: Szigetvári Katalin</p>
+      </footer>
     </div>
   );
 }
